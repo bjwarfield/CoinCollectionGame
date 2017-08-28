125839b0615f64cc43f565bfe9483fdfbd7a31f5
bwarfield <itsabeejay@yahoo.com>
2017-08-28 08:35:35 -0700

    Prototype objects, Materials, Coin Prefabs, scripts

---
+++ CoinCollectionGame/Assets/Scripts/Coin.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+
+    //Keeps trach of total coind count in scene
+    public static int coinCount = 0;
+    public float spinSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+        Coin.coinCount++;
+
+        Debug.Log("Coin instance created.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, spinSpeed * Time.deltaTime);
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GetComponent<AudioSource>().Play();
+            Debug.Log("Entered coin collider");
+            
+            Destroy(gameObject);
+        }
+    }
+
+    //called when object is destroyed
+    void OnDestroy()
+    {
+        //Decrement coin count
+        Coin.coinCount--;
+
+        //check remaining coins
+        if(Coin.coinCount <= 0)
+        {
+            //winning condition
+        }
+        
+    }
+}
---
+++ CoinCollectionGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+
+    public float maxTime = 60f;
+
+    private float countDown = 0;
+
+	// Use this for initialization
+	void Start () {
+        countDown = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countDown -= Time.deltaTime;
+
+        if (countDown <= 0)
+        {
+            Coin.coinCount = 0;
+            //Application.LoadLevel(Application.loadedLevel);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
